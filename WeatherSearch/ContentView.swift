a751383416618be296f28d436dc16339bf77a5cb
Jeff DeWitte <dewitteconsulting@protonmail.com>
2021-03-29 07:59:34 -0400

    Created project

---
+++ WeatherSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherSearch
+//
+//  Created by Jeff DeWitte on 3/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherSearch/WeatherSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSearchApp.swift
+//  WeatherSearch
+//
+//  Created by Jeff DeWitte on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
